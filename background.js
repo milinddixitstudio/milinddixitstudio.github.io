d6c260ecabe18104d533de52fb8a9caa4fda21f1
milinddixitstudio <milinddixit74@gmail.com>
2026-03-09 20:47:55 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,16 @@
-VANTA.WAVES({
-el:"#background",
-mouseControls:true,
-touchControls:true,
-gyroControls:false,
-color:0x222222,
-shininess:40,
-waveHeight:20,
-waveSpeed:1
-})
+const bg=document.getElementById("background")
+
+let hue=200
+
+function animateBG(){
+
+hue+=0.1
+
+bg.style.background=
+`radial-gradient(circle at center, hsl(${hue},30%,15%), #000)`
+
+requestAnimationFrame(animateBG)
+
+}
+
+animateBG()
