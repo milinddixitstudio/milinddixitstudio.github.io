f75d1709bbd036b3cd36d43ce9979ace325788ad
milinddixitstudio <milinddixit74@gmail.com>
2026-03-09 21:50:56 +0530

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -1,6 +1,6 @@
-const bars=document.querySelectorAll(".visualizer div");
+const bars = document.querySelectorAll(".visualizer div");
 setInterval(()=>{
   bars.forEach(bar=>{
-    bar.style.height=Math.random()*50+"px"
+    bar.style.height = Math.random()*50 + "px";
   })
 },120);
