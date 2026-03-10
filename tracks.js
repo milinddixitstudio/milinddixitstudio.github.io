0b28b88dd29f5116e80372b25aec176fc00a9593
milinddixitstudio <milinddixit74@gmail.com>
2026-03-10 19:27:57 +0530

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -1,1 +1,28 @@
+const tracks = [
+
+{
+title:"Lost Within",
+image:"track1.jpg",
+text:"An introspective ambient journey exploring emotional depth."
+},
+
+{
+title:"Frosted Silence",
+image:"track2.jpg",
+text:"A frozen soundscape where silence carries hidden emotion."
+},
+
+{
+title:"Spread Out",
+image:"track3.jpg",
+text:"Music expanding freely into open sonic space."
+},
+
+{
+title:"Trilochana",
+image:"track4.jpg",
+text:"Upcoming release inspired by cosmic perception."
+}
+
+]
 
