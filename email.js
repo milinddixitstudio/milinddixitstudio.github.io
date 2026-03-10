7f61a8d20eadf89530b403806a0847b4fd9a56b4
milinddixitstudio <milinddixit74@gmail.com>
2026-03-10 19:28:49 +0530

    Update email.js

--- email.js
+++ email.js
@@ -1,1 +1,26 @@
+(function(){
+
+emailjs.init("ZGZNv447PhVYVCyDE")
+
+})()
+
+document.getElementById("contact-form")
+.addEventListener("submit",function(e){
+
+e.preventDefault()
+
+emailjs.sendForm(
+"service_a4b3zu3",
+"template_ioithmv",
+this
+)
+
+.then(function(){
+
+alert("Message sent successfully!")
+
+})
+
+})
+
 
